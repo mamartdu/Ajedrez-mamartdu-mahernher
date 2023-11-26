3df070e462bd09b912d5a8f98100900f665b8e89
María Hernani Hernáez <mahernaniher@gmail.com>
2023-11-26 13:23:24 +0100

    Comiezo de partida y servidor

--- src/Juego/Partida.java
+++ src/Juego/Partida.java
@@ -1,11 +1,15 @@
 package Juego;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import setup.Constantes;
 
-public class Partida {
+public class Partida implements Runnable {
+	
 	private Tablero tablero;
 	private Jugador j1 = null, j2 = null;
+	private boolean jaqueMate = false;
 
 	public Partida(Socket s) {
 		if (j1 == null) {
@@ -35,9 +39,42 @@ public class Partida {
 	public void setJugador2(Jugador j2) {
 		this.j2 = j2;
 	}
+	
+	public boolean getJaqueMate() {
+		return jaqueMate;
+	}
+
+	public void setJaqueMate(boolean _jaqueMate ) {
+		this.jaqueMate = _jaqueMate;
+	}
+	
 
 	public void jugar() {
 		tablero = new Tablero();
+		
+		try (	
+			ObjectInputStream inputJugador1=new ObjectInputStream(j1.getSocket().getInputStream());
+			ObjectOutputStream outputJugador1=new ObjectOutputStream(j1.getSocket().getOutputStream());
+			ObjectInputStream inputJugador2=new ObjectInputStream(j2.getSocket().getInputStream());
+			ObjectOutputStream outputJugador2=new ObjectOutputStream(j2.getSocket().getOutputStream());
+			){
+			
+			while (!getJaqueMate()) {
+				outputJugador1.writeObject(tablero);
+				tablero = (Tablero) inputJugador1.readObject();
+				
+			}
+			
+		}catch (Exception e) {
+			
+		}
+	}
+
+	@Override
+	public void run() {
+		if(getJugador1() != null && getJugador2() != null) {
+			jugar();
+		}
 	}
 
 }
--- src/Juego/Servidor.java
+++ src/Juego/Servidor.java
@@ -1,10 +1,39 @@
 package Juego;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class Servidor {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ExecutorService pool = Executors.newCachedThreadPool();
+		ServerSocket socket =null;
+		Socket oConexion = null;
+		Partida oPartida=null;
+		try {
+			socket = new ServerSocket(80);
+			while (true) {
+				try {
+					oConexion = socket.accept();
+					oPartida =new Partida(oConexion);
+					pool.execute(oPartida);
+					
+				} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+				}
+			}
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	
+		}finally {
+			pool.shutdown();
+		}
 	}
-
+	
 }
