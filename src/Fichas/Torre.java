97cc94aa82d40b5bf735b885b7a456a8c3cd7743
mamartdu <mamartdu@unirioja.es>
2023-11-26 11:17:10 +0100

    fichas

---
+++ src/Fichas/Torre.java
@@ -0,0 +1,16 @@
+package Fichas;
+
+public class Torre extends Pieza{
+
+	public Torre(String color) {
+		super(color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean[][] movimientosPosibles(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
