2dbdf5bea0dfe9eea7bb2141eafb6240e8c1c5f2
mamartdu <mamartdu@unirioja.es>
2023-12-01 15:10:31 +0100

    cliente

--- src/Juego/Cliente.java
+++ src/Juego/Cliente.java
@@ -27,6 +27,7 @@ public class Cliente {
 			HashMap<String, Jugador> jugadores = (HashMap<String, Jugador>) in.readObject();	        
 			
 			System.out.println("JUGADORES CONECTADOS:");
+			
 			for (String clave : jugadores.keySet()) {
 			    System.out.println(clave);
 			}
@@ -60,47 +61,5 @@ public class Cliente {
 			e.printStackTrace();
 		}
 		
-		System.out.println(
-				"Si desea esperar a que alguien le solicite una partida pulse 1 y si desea jugar con un usuario conectado concreto pulse 2: ");
-		int opcion = entrada.nextInt();
-		
-		if (opcion == 1) {
-			System.out.println("Introduce la ip con la que se desea conectar: ");
-			String ip = entrada.nextLine();
-			ip = entrada.nextLine();
-			System.out.println("Introduzca su puerto:");
-			int puerto = entrada.nextInt();
-			
-			try (Socket miSocket = new Socket(ip, puerto);
-				ObjectOutputStream outputJugador1 = new ObjectOutputStream(miSocket.getOutputStream());
-				ObjectInputStream inputJugador1 = new ObjectInputStream(miSocket.getInputStream());) {
-				
-				System.out.println("Ya estamos conectados");
-				Juego juego = new Juego();
-				juego.colocarFichasIniciales();
-				Interfaz tablero = new Interfaz(juego);
-				
-				while(!tablero.finPartida()) {	
-					
-					outputJugador1.writeObject(tablero);
-					tablero = (Interfaz) inputJugador1.readObject();
-					if(juego.isTurnoNegras()) {
-						//MUEVEN NEGRAS
-						juego.muevesPieza();	
-						tablero.bloquear();
-						outputJugador1.writeObject(tablero);
-						outputJugador1.flush();
-					}
-					
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}else {
-			
-		}
 	}
 }
