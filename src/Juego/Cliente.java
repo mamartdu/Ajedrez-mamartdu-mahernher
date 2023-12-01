63e5b1068dd4329412974780887854cc63a75416
mamartdu <mamartdu@unirioja.es>
2023-12-01 15:48:22 +0100

    cambios cliente

--- src/Juego/Cliente.java
+++ src/Juego/Cliente.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,7 +17,8 @@ public class Cliente {
 		String nombre = entrada.nextLine();
 		System.out.println("Introduce tu puerto");
 		int puerto = entrada.nextInt();
-		
+		HashMap<String, Jugador> jugadores = null;
+
 		try (Socket s = new Socket("localhost", 6666);
 				DataOutputStream dos = new DataOutputStream(s.getOutputStream());) {
 			dos.writeBytes(nombre+"\n");
@@ -24,32 +26,11 @@ public class Cliente {
 			dos.flush();
 			ObjectInputStream in = new ObjectInputStream(s.getInputStream());
 			
-			HashMap<String, Jugador> jugadores = (HashMap<String, Jugador>) in.readObject();	        
+			jugadores = (HashMap<String, Jugador>) in.readObject();	 	        
 			
 			System.out.println("JUGADORES CONECTADOS:");
-			
 			for (String clave : jugadores.keySet()) {
 			    System.out.println(clave);
-			}
-			
-			System.out.println("Crear mi partida pulse 1");
-			System.out.println("Unirse a un jugador pulse 2");
-			String opcion = entrada.nextLine();
-			
-			switch (opcion) {
-				case "1":
-					
-					try (ServerSocket ss = new ServerSocket(puerto);
-						ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
-						ObjectInputStream ois = new ObjectInputStream(s.getInputStream());) {
-						
-						Juego juego = new Juego();
-						Interfaz i =new Interfaz(juego);
-						i.mostrarTablero();
-						
-						
-					}
-				case "2":
 			}
 			
 			s.close();
@@ -61,5 +42,49 @@ public class Cliente {
 			e.printStackTrace();
 		}
 		
+		System.out.println(
+				"Si desea esperar a que alguien le solicite una partida pulse 1 y si desea jugar con un usuario conectado concreto pulse 2: ");
+		int opcion = entrada.nextInt();
+		
+		if (opcion == 1) {
+			try (ServerSocket ss = new ServerSocket(puerto);) {
+
+				while (true) {
+					try (Socket s = ss.accept();) {
+						ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+						ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+						
+						System.out.println(ois.readLine());
+					
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}else {
+			System.out.println("Introduce la jugador con la que se desea conectar: ");
+			entrada.next();
+			String nombreAux = entrada.nextLine();
+			Jugador jugador = jugadores.get(nombreAux);
+			System.out.print(jugador);			
+			try (Socket sPersonal = new Socket(jugadores.get(nombreAux).getIp(), jugadores.get(nombreAux).getPuerto());
+							ObjectOutputStream oos = new ObjectOutputStream(sPersonal.getOutputStream());
+							ObjectInputStream ois = new ObjectInputStream(sPersonal.getInputStream());) {
+				
+				oos.writeBytes("me conecto\n");
+				
+				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
